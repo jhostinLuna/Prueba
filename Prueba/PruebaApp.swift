81597e9810141447ea197414ea8f27d70a8c9fff
Jhostin Luna <jhostinlunadev@gmail.com>
2024-03-31 09:43:47 +0200

    Initial Commit

---
+++ Prueba/PruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PruebaApp.swift
+//  Prueba
+//
+//  Created by Jhostin on 31/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
